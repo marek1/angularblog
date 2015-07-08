ace9cacf1fdba64138415060021d5962daf57cb4
marek1 <cyclingfellas@gmail.com>
2015-07-08 11:20:56 +0200

    HTML5 Mode

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,19 +1,21 @@
 var angularblog = angular.module('angularblog', [])
-.config(['$routeProvider', function($routeProvider,$locationProvider) {
-	
+  .config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
+
     'use strict';
 
-	$routeProvider.when('/', {
-		templateUrl : 'views/main.html',
-		controller : 'MainController'
-	}).when('/blog/:url', {
-		templateUrl : 'views/blog.html',
-		controller : 'BlogController'
-	}).when('/add', {
-		templateUrl : 'views/blog.html',
-		controller : 'AddController'
-	}).otherwise({
-		redirectTo : '/'
-	});
-	
-}]);
+    $locationProvider.html5Mode(true);
+
+    $routeProvider.when('/', {
+      templateUrl: 'views/main.html',
+      controller: 'MainController'
+    }).when('/blog/:url', {
+      templateUrl: 'views/blog.html',
+      controller: 'BlogController'
+    }).when('/add', {
+      templateUrl: 'views/blog.html',
+      controller: 'AddController'
+    }).otherwise({
+      redirectTo: '/'
+    });
+
+  }]);
